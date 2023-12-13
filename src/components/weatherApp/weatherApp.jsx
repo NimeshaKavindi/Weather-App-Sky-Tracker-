6142a1bd6e9500b247d2cccf1a0aea0aa06219a8
Nimesha Kavindi <abeykoo-se19021@stu.kln.ac.lk>
2023-12-13 12:55:07 +0530

    get the city temperature, humidity, wind with wweather map link

--- src/components/weatherApp/weatherApp.jsx
+++ src/components/weatherApp/weatherApp.jsx
@@ -14,14 +14,26 @@ const WeatherApp = () => {
 
     let api_key = "c700b511e6d72e8fb13fa6a0a3fa146f";
 
-    const search =() =>{
+    const search = async() =>{
        const element = document.getElementsByClassName("cityInput")
        if(element[0].value ===""){
         return 0;
        }
 
        let url =`https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
-       
+
+       let response = await fetch(url);
+       let data = await response.json();
+       const humidity = document.getElementsByClassName("humidity");
+       const wind = document.getElementsByClassName("wind-speed");
+       const temperature = document.getElementsByClassName("weather-temp");
+       const location = document.getElementsByClassName("weather-location");
+
+       humidity[0].innerHTML = data.main.humidity;
+       wind[0].innerHTML = data.wind.speed;
+       temperature[0].innerHTML = data.main.temp;
+       location[0].innerHTML = data.name;
+
     }
     return (
       <div className='container'>
